f11658653a34b226432f86acb1d19da20998cd3c
AkimotoHaruma <siba16fb@gmail.com>
2022-04-05 10:57:10 +0000

    only ir and coloer stream enabled

--- script/pyrealsense.py
+++ script/pyrealsense.py
@@ -15,10 +15,9 @@ if __name__ == '__main__':
         #https://intelrealsense.github.io/librealsense/python_docs/_generated/pyrealsense2.config.html#pyrealsense2.config.enable_stream
         config = rs.config()
         config.enable_stream(rs.stream.color, WIDTH, HEIGHT, rs.format.bgr8, 30)
-        config.enable_stream(rs.stream.depth, WIDTH, HEIGHT, rs.format.z16, 30)
+        #config.enable_stream(rs.stream.depth, WIDTH, HEIGHT, rs.format.z16, 30)
         config.enable_stream(rs.stream.infrared, 1, WIDTH, HEIGHT, rs.format.y8, 6)
         config.enable_stream(rs.stream.infrared, 2, WIDTH, HEIGHT, rs.format.y8, 6)
-
 
         pipeline = rs.pipeline()
         pipeline.start(config)
@@ -28,26 +27,27 @@ if __name__ == '__main__':
 
             #https://intelrealsense.github.io/librealsense/python_docs/_generated/pyrealsense2.composite_frame.html#pyrealsense2-composite-frame
             color_frame = frames.get_color_frame()
-            depth_frame = frames.get_depth_frame()
+            #depth_frame = frames.get_depth_frame()
             ir1_frame = frames.get_infrared_frame(1)
             ir2_frame = frames.get_infrared_frame(2)
 
-            if not color_frame or not depth_frame or not ir1_frame or not ir2_frame:
+            #if not color_frame or not depth_frame or not ir1_frame or not ir2_frame:
+            if not ir1_frame or not ir2_frame:
                 continue
             # translation to OpenCV Format
             color_image = np.asanyarray(color_frame.get_data())
-            depth_image = np.asanyarray(depth_frame.get_data())
+            #depth_image = np.asanyarray(depth_frame.get_data())
             ir1_image = np.asanyarray(ir1_frame.get_data())
             ir2_image = np.asanyarray(ir2_frame.get_data())
             # depth image to Colormap
-            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.08), cv2.COLORMAP_JET)      
+           # depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.08), cv2.COLORMAP_JET)      
 
             # Show Color images
             cv2.namedWindow('color_image', cv2.WINDOW_AUTOSIZE)
             cv2.imshow('color_image', color_image)
             # Show Depth images
-            cv2.namedWindow('depth_image', cv2.WINDOW_AUTOSIZE)
-            cv2.imshow('depth_image', depth_colormap)
+            #cv2.namedWindow('depth_image', cv2.WINDOW_AUTOSIZE)
+            #cv2.imshow('depth_image', depth_colormap)
 
             # Show Ir1 images
             cv2.namedWindow('ir1_image', cv2.WINDOW_AUTOSIZE)
